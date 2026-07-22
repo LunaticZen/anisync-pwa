aa4ca317d1f54f7ff69310e9bacb2fc18b1dab0c
LunaticZen <75453644+LunaticZen@users.noreply.github.com>
2026-07-22 17:53:22 +0300

    chore: bump mobile app cache buster to v6

--- packages/mobile/app/src/main/java/com/anisync/mobile/MainActivity.java
+++ packages/mobile/app/src/main/java/com/anisync/mobile/MainActivity.java
@@ -57,7 +57,7 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "AniSync";
-    private static final String SERVER_URL = "https://anisync-9z1z.onrender.com/?v=4";
+    private static final String SERVER_URL = "https://anisync-9z1z.onrender.com/?v=6";
     private static final int FILE_CHOOSER_REQUEST = 1001;
 
     // Mobil video senkronizasyonunu (CORS bypass - HTML Interception) açıp kapatan ana şalter
